ec06603342f87e4f3ee4aba52381b6fdd2d7ae56
sharpenedblade <sharpenedblade@proton.me>
2023-05-06 14:56:24 -0700

    fix: Fix error handling to panic! less

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ use glob::glob;
 use std::fs;
 use std::path::Path;
 use std::path::PathBuf;
-use std::u32;
+use std::process::ExitCode;
 
 enum FanCurve {
     LINEAR,
@@ -32,31 +32,27 @@ struct Fan {
 }
 
 impl Fan {
-    fn new(path: PathBuf) -> Fan {
+    fn new(path: PathBuf) -> Result<Fan, std::io::Error> {
         let fan = Fan {
-            max_speed: fs::read_to_string(Path::join(&path, "_max"))
-                .expect("Failed to read max speed")
-                .parse()
-                .expect("Failed to parse max speed"),
-            min_speed: fs::read_to_string(Path::join(&path, "_min"))
-                .expect("Failed to read min speed")
-                .parse()
-                .expect("Failed to parse min speed"),
+            max_speed: fs::read_to_string(Path::join(&path, "_max"))?
+                .parse::<u32>()
+                .unwrap(), // This file will always be an int
+            min_speed: fs::read_to_string(Path::join(&path, "_min"))?
+                .parse::<u32>()
+                .unwrap(), // Same as above
             path,
-            speed_curve: FanCurve::LINEAR,
+            speed_curve: FanCurve::LINEAR, // TODO: Stop hardcoding fan cruve
         };
-        fs::write(Path::join(&fan.path, "_manual"), "1")
-            .expect("Failed to enable manual fan control");
-        return fan;
+        fs::write(Path::join(&fan.path, "_manual"), "1")?;
+        return Ok(fan);
     }
 
-    fn set_speed(&self, speed: u32) {
+    fn set_speed(&self, speed: u32) -> Result<(), std::io::Error> {
         fs::write(Path::join(&self.path, "_output"), speed.to_string())
-            .expect("Failed to set fan speed");
     }
 
     fn calc_speed(&self, current_temp: u32) -> u32 {
-        let min_temp: u32 = 80;
+        let min_temp: u32 = 80; // TODO: Stop hardcoding temp limits
         let max_temp: u32 = 100;
         match self.speed_curve {
             FanCurve::LINEAR => {
@@ -68,28 +64,30 @@ impl Fan {
     }
 }
 
-fn init_fans() -> Vec<Fan> {
+fn init_fans() -> Result<Vec<Fan>, std::io::Error> {
     let mut all_fans = Vec::new();
-    // TODO: Remove "_input" from file name, currently BROKEN
-    for i in glob("/sys/devices/*/*/*/*/APP0001:00/fan*").expect("Failed to locate fan") {
-        all_fans.push(Fan::new(i.expect("Error")))
+    for i in glob("/sys/devices/*/*/*/*/APP0001:00/fan*_input").unwrap() {
+        // TODO: Strip `_input` from file name
+        all_fans.push(Fan::new(i.unwrap())?); // Glob is always readable
     }
-    if all_fans.len() == 0 {
-        panic!("Fan objects failed to initialize");
-    }
-    return all_fans;
+    return Ok(all_fans);
 }
 
 fn get_current_temp() -> u32 {
+    // TODO: Resolve glob pattern
     let cpu_temp_path = PathBuf::from("/sys/devices/platform/coretemp.0/hwmon/hwmon*/temp1_input");
-    let cpu_temp: String =
-        fs::read_to_string(cpu_temp_path).expect("Failed to get CPU temperature");
-    let cpu_temp: u32 = cpu_temp.parse().expect("Failed to parse CPU temperature");
+    let cpu_temp: String = match fs::read_to_string(cpu_temp_path) {
+        Ok(temp) => temp,
+        Err(..) => panic!("Failed to read CPU temp. Are you running as root?"),
+    };
+    let cpu_temp: u32 = cpu_temp.parse::<u32>().unwrap(); // Always parsable
 
     let gpu_temp_path = PathBuf::from("/sys/class/drm/card0/device/hwmon/hwmon*/temp1_input");
-    let gpu_temp: String =
-        fs::read_to_string(gpu_temp_path).expect("Failed to get GPU temperature");
-    let gpu_temp: u32 = gpu_temp.parse().expect("Failed to parse GPU temperature");
+    let gpu_temp: String = match fs::read_to_string(gpu_temp_path) {
+        Ok(temp) => temp,
+        Err(..) => panic!("Failed to read GPU temp. Are you running as root?"),
+    };
+    let gpu_temp: u32 = gpu_temp.parse::<u32>().unwrap(); // Same as above
 
     if gpu_temp > cpu_temp {
         gpu_temp
@@ -98,11 +96,21 @@ fn get_current_temp() -> u32 {
     }
 }
 
-fn main() {
-    let fans = init_fans();
+fn main() -> ExitCode {
+    let fans = match init_fans() {
+        Ok(fans) => fans,
+        Err(..) => panic!("An error occured when initializing fans"),
+    };
+    if fans.len() == 0 {
+        println!("No fans found");
+        return ExitCode::from(1);
+    }
     loop {
         for fan in &fans {
-            fan.set_speed(fan.calc_speed(get_current_temp()));
+            match fan.set_speed(fan.calc_speed(get_current_temp())) {
+                Ok(..) => continue,
+                Err(..) => println!("Error: Failed to set fan speed"),
+            }
         }
     }
 }
